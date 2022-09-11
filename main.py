c181e6808767f881a5491a6c95bb13082ce05d28
Sugobet <sugob@qq.com>
2022-09-11 11:16:26 +0800

    优化内存占用问题

--- main.py
+++ main.py
@@ -632,7 +632,7 @@ def Start(device_name, device_address, cnocr):
 
 
 if __name__ == '__main__':
+    cnocr = CnOcr(rec_model_name='densenet_lite_136-gru')
     for key, val in devices.items():
-        cnocr = CnOcr(rec_model_name='densenet_lite_136-gru')
         t = threading.Thread(target=Start, args=(key, val, cnocr))
         t.start()
